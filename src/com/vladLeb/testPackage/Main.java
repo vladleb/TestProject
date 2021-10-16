b9555ad755552d14606415635131ef2d4f7e8024
Lebedev Vladislav <vladis90@bk.ru>
2021-10-16 12:59:01 +0300

    new commit

--- src/com/vladLeb/testPackage/Main.java
+++ src/com/vladLeb/testPackage/Main.java
@@ -31,6 +31,7 @@ public class Main
                 Arrays.stream(test).boxed().collect(Collectors.toList()));
 //        for(int i = 0; i < test.length; i++)
 //            testSet.add(test[i]);
+        System.out.println(Arrays.stream(test).distinct().boxed().collect(Collectors.toList()).size());
 
         System.out.println(test.length);
         System.out.println(testSet.size());
@@ -87,7 +88,84 @@ public class Main
 
         //System.out.println(Arrays.toString());
 
+        ///////////Найти самый часто встречающийся символ в строке///////////
 
+        String input = "Hellooo";
+        input = input.toLowerCase();
+        int max = 0;
+        int diff;
+        char maxChar = '\0'; // in case it's an empty string, ignore if you don't understand
+        for(char c = 'a'; c <= 'z'; c++) {
+            diff = input.length() - input.replace("" + c, "").length();
+            if(diff > max) {
+                maxChar = c;
+                max = diff;
+            }
+        }
+        System.out.println("MaxChar = " + maxChar);
+
+
+        String text =   "aabaabaaaaabaabaabaabbaaab";
+        String sample = "aabaabc";
+
+        System.out.println(Arrays.toString(searchNaive(text, sample).toArray()));
+        System.out.println(Arrays.toString(prefixFunction(sample)));
+        System.out.println(Arrays.toString(KMPSearch(text, sample).toArray()));
+
+    }
+
+    static ArrayList<Integer> searchNaive(String text, String sample) {
+        ArrayList<Integer> foundPositions = new ArrayList<>();
+        for (int i = 0; i < text.length(); i++) {
+            int j = 0;
+            while (j < sample.length() && i + j < text.length() && sample.charAt(j) == text.charAt(i + j)) {
+                j++;
+            }
+            if (j == sample.length()) {
+                foundPositions.add(i);
+            }
+        }
+        return foundPositions;
+    }
+
+    static int[] prefixFunction(String sample) {
+        int [] values = new int[sample.length()];
+        for (int i = 1; i < sample.length(); i++) {
+            int j = 0;
+            while (i + j < sample.length() && sample.charAt(j) == sample.charAt(i + j)) {
+                values[i + j] = Math.max(values[i + j], j + 1);
+                j++;
+            }
+        }
+        return values;
+    }
+
+    public static ArrayList<Integer> KMPSearch(String text, String sample) {
+        ArrayList<Integer> found = new ArrayList<>();
+
+        int[] prefixFunc = prefixFunction(sample);
+
+        int i = 0;
+        int j = 0;
+
+        while (i < text.length()) {
+            if (sample.charAt(j) == text.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == sample.length()) {
+                found.add(i - j);
+                j = prefixFunc[j - 1];
+            } else if (i < text.length() && sample.charAt(j) != text.charAt(i)) {
+                if (j != 0) {
+                    j = prefixFunc[j - 1];
+                } else {
+                    i = i + 1;
+                }
+            }
+        }
+
+        return found;
     }
 
 
